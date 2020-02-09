0680c5b85a0f7af0773acaad588846909cf9c52e
Xu Ren <xu.ren@stonybrook.edu>
2020-02-09 17:17:02 -0500

    updated

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
 requirements = ['Click>=7.0', 'numpy', 'pandas', 'mygene', 'statsmodels',
-                'missingpy', 'seaborn', 'matplotlib']
+                'missingpy', 'seaborn', 'matplotlib', "sklearn"]
 
 setup_requirements = ['pytest-runner']
 
